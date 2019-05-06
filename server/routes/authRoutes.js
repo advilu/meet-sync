887fa7c803d934acd7fb497f59d5a8667224a13f
Adrienne Lundry <adriennelundry@Adriennes-MacBook-Pro-2.local>
2019-05-06 12:11:36 -0700

    manifest, the sleeper file

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,7 +6,20 @@ router.get('/google', passport.authenticate('google', {
 }));
 
 router.get('/google/callback', passport.authenticate('google'), (req, res) => {
-    res.redirect('http://localhost:3000/');
+    if(process.env.NODE_ENV === 'production') {
+        res.redirect('/')
+    } else {
+        res.redirect('http://localhost:3000/');
+    }
+    
 });
+
+router.get('/zoom/callback'), (req, res) => {
+    if(process.env.NODE_ENV === 'production') {
+        res.redirect('/')
+    } else {
+        res.redirect('http://localhost:3000/');
+    }
+}
 
 module.exports = router;
